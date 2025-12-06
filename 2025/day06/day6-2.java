5f8e12438d6b257baba0fc140cb0442a113d9ff5
HusrevAkbas <husrev.akbas@gmail.com>
2025-12-06 22:14:03 +0100

    feat: 2025/day06 2. puzzle solved

--- 2025/day06/day6-2.java
+++ 2025/day06/day6-2.java
@@ -20,40 +20,88 @@ public class day6
 			String input = Files.readString(Path.of(filename));
 
             String[] lines = input.split("\n");
-            ArrayList<String[]> allOps = new ArrayList<String[]>();
-            for (String line : lines)
-            {
-                // System.out.println("line " + line);
-                String[] splitted = line.trim().split("\\s+");
-                // System.out.println(Arrays.toString(splitted));
-                allOps.add(splitted);
-            }
 
+            // For second puzzle I must keep whitespaces and work char by char
+
+            // ArrayList<String[]> allOps = new ArrayList<String[]>();
+            // for (String line : lines)
+            // {
+            //     // System.out.println("line " + line);
+            //     String[] splitted = line.trim().split("\\s+");
+            //     // System.out.println(Arrays.toString(splitted));
+            //     allOps.add(splitted);
+            // }
+
+            ArrayList<String> numbers = new ArrayList<>();
             long result = 0;
-
-            String[] ops = allOps.get(allOps.size() - 1);
-            for (int i = 0; i < ops.length; i++)
+            
+            for (int i = lines[0].length() - 1; i >= 0; i--)
             {
-                long part = ops[i].equals("*") ? 1 : 0;
-                for (int a = 0; a < allOps.size() - 1; a++)
+                String op = "";
+                String num = "";
+                long partial_result = 0;
+
+                for (String l : lines)
                 {
-                    // multiply
-                    if (ops[i].equals("*"))
+                    if (Character.isDigit(l.charAt(i)))
+                        num += l.substring(i,i+1);
+                    if (l.charAt(i) == ('*'))
                     {
-                        part *= Long.parseLong(allOps.get(a)[i]);
+                        op = "*";
                     }
-                    else // add
+                    if (l.charAt(i) == ('+'))
                     {
-                        part += Long.parseLong(allOps.get(a)[i]);
+                        op = "+";
                     }
+
                 }
-                result += part;
+                if (num.isEmpty())
+                    continue;
+                
+                numbers.add(num);
+
+                if (op.equals("*"))
+                {
+                    result += multistr(numbers);
+                    numbers.clear();
+
+                }
+                if (op.equals("+"))
+                {
+                    result += sumstr(numbers);
+                    numbers.clear();
+
+                }
             }
 
-            System.out.println(result);
+            System.out.println("Solution is : " + result);
 
         } catch (Exception e) {
             System.out.println("Something is wrong");
         }
+    }
+
+    public static long sumstr(ArrayList<String> numbers)
+    {
+        long result = 0;
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            System.out.print(" + " + numbers.get(i));
+            result += Long.parseLong(numbers.get(i));
+        }
+        System.out.println(" = " + result);
+        return result;
+    }
+
+    public static long multistr(ArrayList<String> numbers)
+    {
+        long result = 1;
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            System.out.print(" * " + numbers.get(i));
+            result *= Long.parseLong(numbers.get(i));
+        }
+        System.out.println(" = " + result);
+        return result;
     }
 }
